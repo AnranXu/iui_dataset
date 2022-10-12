429c8c033bc9df8ce207b2e9f877049d9f6254a9
AnranXu <anran1998@126.com>
2022-10-12 21:51:06 +0900

    Create change_importance_to_imformative.py

---
+++ results/change_importance_to_imformative.py
@@ -0,0 +1,20 @@
+import os
+
+if __name__ == '__main__':
+    crowdworks_labels = os.listdir(os.path.join('CrowdWorks', 'crowdscouringlabel'))
+    prolific_labels = os.listdir(os.path.join('Prolific', 'crowdscouringlabel'))
+    root = os.path.join('CrowdWorks\crowdscouringlabel')
+    files = os.listdir(root)
+    text = ''
+    for file in crowdworks_labels:
+        with open(os.path.join(root, file), encoding='utf-8') as f:
+            text = f.read()
+            text = text.replace('importance', 'informativeness')
+        with open(os.path.join(root, file), 'w', encoding='utf-8') as f:
+            f.write(str(text))
+    for file in prolific_labels:
+        with open(os.path.join(root, file), encoding='utf-8') as f:
+            text = f.read()
+            text = text.replace('importance', 'informativeness')
+        with open(os.path.join(root, file), 'w', encoding='utf-8') as f:
+            f.write(str(text))
