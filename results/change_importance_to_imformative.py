9503561449bfdc0b593e14a7f20e7dfdbaee9eff
AnranXu <anran1998@126.com>
2022-10-13 11:01:44 +0900

    data handler

--- results/change_importance_to_imformative.py
+++ results/change_importance_to_imformative.py
@@ -3,8 +3,7 @@ import os
 if __name__ == '__main__':
     crowdworks_labels = os.listdir(os.path.join('CrowdWorks', 'crowdscouringlabel'))
     prolific_labels = os.listdir(os.path.join('Prolific', 'crowdscouringlabel'))
-    root = os.path.join('CrowdWorks\crowdscouringlabel')
-    files = os.listdir(root)
+    root = os.path.join('CrowdWorks', 'crowdscouringlabel')
     text = ''
     for file in crowdworks_labels:
         with open(os.path.join(root, file), encoding='utf-8') as f:
@@ -12,6 +11,7 @@ if __name__ == '__main__':
             text = text.replace('importance', 'informativeness')
         with open(os.path.join(root, file), 'w', encoding='utf-8') as f:
             f.write(str(text))
+    root = os.path.join('Prolific', 'crowdscouringlabel')
     for file in prolific_labels:
         with open(os.path.join(root, file), encoding='utf-8') as f:
             text = f.read()
---
+++ results/generate_file_for_publish.py
@@ -0,0 +1,6 @@
+import os 
+import json
+
+if __name__ == '__main__':
+    ## we need to make a version for publication that hide worker's id in the file
+    pass
